cea3b676804cbdd61d125cc75f92b43e99acecc3
Bruno Lages <bruno.lages@feedzai.com>
2022-04-06 11:27:55 +0100

    Add Cypress Test

--- cypress/integration/01-test/addtask.test.js
+++ cypress/integration/01-test/addtask.test.js
@@ -9,48 +9,54 @@ const MENU = {
 
 const ADDTASK = {
   SHOWACTIONS_BTN: "[data-testid='show-main-action']",
-  SUBMIT_BTN: "[data-testid='add-task__submit']",
+  SUBMIT_BTN: "[data-testid='add-task']",
   CANCEL_BTN: "[data-testid='add-task-main-cancel']",
+  TXT: "[data-testid='add-task-content']",
   PROJECT_MENU_BTN: "[data-testid='show-project-overlay']",
-  DATE_MNU: {
+  DATE_MENU: {
       BTN: "[data-testid='show-task-date-overlay']",
       TOMORROW_VAL: "[data-testid='task-date-tomorrow']",
+      NEXTWEEK_VAL: "[data-testid='task-date-next-week']",
   },
 };
 
 const TASK_CHK = "[data-testid='checkbox-action']";
 
-
 describe('Todoist - Add Task', () => {
   beforeEach(() => {
     cy.visit('/');
-  })
+})
 
-  it('Add Task for Tomorrow', () => {
+afterEach(() => {
+  cy.get(MENU.NEXT7DAYS_BTN).click();
+  cy.get(TASK_CHK).each((task) => {
+    cy.wrap(task).click();
+  })
+})
+  it('Add Task for Next Week', () => {
     // 1.1 - Click on Add Task Plus (Show actions) button
     cy.get(PAGETITLE_LBL).contains("Inbox");
     cy.get(ADDTASK.SHOWACTIONS_BTN).click();
 
     // 1.2 - Check all buttons and click on Date button (menu)
-    cy.get(ADDTASK.SUBMIT_BTN).should("be.visible").and("be.enabled");
-    cy.get(ADDTASK.CANCEL_BTN).should("be.visible").and("be.enabled");
-    cy.get(ADDTASK.PROJECT_MNU).should("be.visible").and("be.enabled");
-    cy.get(ADDTASK.DATE_MNU.BTN).should("be.visible").and("be.enabled").click();
+    cy.get(ADDTASK.SUBMIT_BTN).should("be.visible");
+    cy.get(ADDTASK.CANCEL_BTN).should("be.visible");
+    cy.get(ADDTASK.PROJECT_MENU_BTN).should("be.visible");
+    cy.get(ADDTASK.DATE_MENU.BTN).should("be.visible").click();
 
-    // 2.1 - Click on Tomorrow value (menu)
-    cy.get(ADDTASK.DATE_MNU.TOMORROW_VAL).click();
+    // 2.1 - Click on Next Week dropdown (menu)
+    cy.get(ADDTASK.DATE_MENU.NEXTWEEK_VAL).click();
 
     // 2.2 - Fill task field and click on Add Task (Submit) button
-    cy.get("[data-testid='add-task-content']").should("be.visible").type("Check tasks for the next sprint");
+    cy.get(ADDTASK.TXT).should("be.visible").type("Check tasks for the next sprint");
     cy.get(ADDTASK.SUBMIT_BTN).click();
 
     // 2.3 - Check that the task does not appear for today
-    cy.get(TASK_CHK).should("not.be.visible");
+    cy.get(TASK_CHK).should("not.exist");
 
     // 3.1 - Click on "Next 7 days" menu and check task created
     cy.get(MENU.NEXT7DAYS_BTN).click();
     cy.get(PAGETITLE_LBL).contains("Next 7 Days");
-    cy.get(TASK_CHK).contains("Check tasks for the next sprint");
-  })
-
-})
+    cy.get(TASK_CHK).eq(0).parent().contains("Check tasks for the next sprint");
+  });
+});
